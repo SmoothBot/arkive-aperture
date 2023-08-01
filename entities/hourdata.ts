cc963f237f5d629c7c388b0648b6087736d27ec2
SmoothBot <smoothbot123@gmail.com>
2023-08-01 21:34:18 +1000

    Adding gmx emissions

--- entities/hourdata.ts
+++ entities/hourdata.ts
@@ -3,12 +3,22 @@ import { createEntity } from "https://deno.land/x/robo_arkiver/src/graphql/entit
 
 interface IHourData {
 	timestamp: number,
+	glpTotalSupply: number,
+	gmxCumulativeRewards: number,
+	gmxEmissions: number,
+	gmxEmissionsUsd: number,
+	gmxPrice: number,
 	glpPrice: number,
 	ethPrice: number,
 }
 
 export const HourData = createEntity<IHourData>("HourData", {
 	timestamp: { type: Number, index: true },
+	glpTotalSupply: Number,
+	gmxCumulativeRewards: Number,
+	gmxEmissions: Number,
+	gmxEmissionsUsd: Number,
+	gmxPrice: Number,
 	glpPrice: Number,
 	ethPrice: Number,
 })
--- manifest.ts
+++ manifest.ts
@@ -1,11 +1,9 @@
-import { Manifest } from "https://deno.land/x/robo_arkiver@v0.4.7/mod.ts";
+import { Manifest } from "https://deno.land/x/robo_arkiver@v0.4.19/mod.ts";
 import { hourDataHandler } from "./handlers/hourdata.ts";
 import { HourData } from "./entities/hourdata.ts";
 import { OptionMarketAbi } from "./abis/OptionMarketAbi.ts";
 import { onTrade } from "./handlers/trade.ts";
 import { Trade } from "./entities/trade.ts";
-import { glpRewardAbi } from "../abis/GLPRewardAbi.js";
-import { onGLPEvent } from "./handlers/glprewards.js";
  
 
 
@@ -15,16 +13,14 @@ const startBlockHeight = 54905918n
 const manifest = new Manifest('aperture');
 const arbitrum = manifest
 	.addEntities([HourData, Trade])
-	.chain("arbitrum")
+	.addChain("arbitrum")
 
 arbitrum
-	.addBlockHandler({ blockInterval: 100, startBlockHeight, handler: hourDataHandler })
+	.addBlockHandler({ blockInterval: 1000, startBlockHeight, handler: hourDataHandler })
 
 arbitrum
-  .contract('OptionMarket', OptionMarketAbi)
-  .contract('GLPReward', glpRewardAbi)
+  .addContract('OptionMarket', OptionMarketAbi)
   .addSources({ '0x919E5e0C096002cb8a21397D724C4e3EbE77bC15': 54905918n })
   .addEventHandlers({ 'Trade': onTrade })
-  .addEventHandlers({'GLPEvent': onGLPEvent})
 
 export default manifest.build();
